278a9a5fbf0fc3e46780a266ac01fc22f531342a
cxy592394546 <cxy592394546@gmail.com>
2021-02-19 18:05:42 +0800

    add

--- Code/Q11/minArray.java
+++ Code/Q11/minArray.java
@@ -5,5 +5,12 @@ public class minArray {
         System.out.println("Hello World!");
     }
 
-
+    public int minArray(int[] numbers) {
+        for(int i = 1; i < numbers.length; i++){
+            if(numbers[i] < numbers[i - 1]){
+                return numbers[i];
+            }
+        }
+        return numbers[0];
+    }
 }
