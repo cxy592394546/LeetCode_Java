678fd77443fa625e4a2cdc13c1dcf84afa256f69
cxy592394546 <cxy592394546@gmail.com>
2021-02-23 20:50:51 +0800

    add
    add

---
+++ Code/Q13/movingCount.java
@@ -0,0 +1,18 @@
+package Q13;
+
+public class movingCount { // dfs
+    boolean[][] map;
+
+    public int movingCount(int m, int n, int k) {
+        map = new boolean[m][n];
+        return count(0, 0, m, n, k);
+    }
+
+    private int count(int x, int y, int m, int n, int k) {
+        if (x >= m || y >= n || x % 10 + y % 10 + x / 10 + y / 10 > k || map[x][y] == true) {
+            return 0;
+        }
+        map[x][y] = true;
+        return 1 + count(x + 1, y, m, n, k) + count(x, y + 1, m, n, k);
+    }
+}
---
+++ Code/Q16/myPow.java
@@ -0,0 +1,7 @@
+package Q16;
+
+public class myPow {
+    public static void main(String[] args) {
+        System.out.println(8&-8);
+    }
+}
