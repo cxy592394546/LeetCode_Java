bfac927cde5599d2e62306b28bd27edde6642a48
cxy592394546 <cxy592394546@gmail.com>
2021-03-01 11:57:20 +0800

    add

--- Code/Q16/myPow.java
+++ Code/Q16/myPow.java
@@ -1,7 +1,19 @@
 package Q16;
 
 public class myPow {
-    public static void main(String[] args) {
-        System.out.println(8&-8);
+    public double myPow(double x, int n) {
+        int base = 1;
+        x = n > 0? x: 1/x;
+        double ret = 1.0;
+        n = Math.abs(n);
+        while(n != 0){
+            if(base == (n & -n)){
+                n -= (n & -n);
+                ret *= x;
+            }
+            x *= x;
+            base *= 2;
+        }
+        return ret;
     }
 }
---
+++ Code/Q19/isMatch.java
@@ -0,0 +1,4 @@
+package Q19;
+
+public class isMatch {
+}
